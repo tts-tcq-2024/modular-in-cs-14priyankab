573cb2afe8672d142034bd5d3a1a58df57ebca47
Priyanka B <86424906+14priyankab@users.noreply.github.com>
2024-10-10 00:16:41 +0530

    Update ColorPairMapperTests.cs

--- ColorPairMapperTests.cs
+++ ColorPairMapperTests.cs
@@ -25,4 +25,24 @@ namespace TelCo.ColorCoder
             VerifyNumberMapping(_mapper, System.Drawing.Color.Violet, System.Drawing.Color.SlateGray, 25);
         }
     }
+
+
+ public abstract class ColorPairMapperTestsBase
+    {
+        protected void VerifyColorPairMapping(ColorPairMapperBase mapper, int pairNumber, Color expectedMajor, Color expectedMinor)
+        {
+            ColorPair testPair = mapper.MapNumberToColorPair(pairNumber);
+            Console.WriteLine($"[In] Pair Number: {pairNumber}, [Out] Colors: {testPair}");
+            Debug.Assert(testPair.MajorColor == expectedMajor);
+            Debug.Assert(testPair.MinorColor == expectedMinor);
+        }
+
+        protected void VerifyNumberMapping(ColorPairMapperBase mapper, Color majorColor, Color minorColor, int expectedPairNumber)
+        {
+            ColorPair testPair = new ColorPair { MajorColor = majorColor, MinorColor = minorColor };
+            int pairNumber = mapper.MapColorPairToNumber(testPair);
+            Console.WriteLine($"[In] Colors: {testPair}, [Out] PairNumber: {pairNumber}");
+            Debug.Assert(pairNumber == expectedPairNumber);
+        }
+    }
 }
